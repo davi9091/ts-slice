1e3754544c94b5f4ce9a1a5a23b28a3e26146fb5
Arkadii <davi9091@pop-os.localdomain>
2023-07-14 02:26:05 +0400

    added reduce function, added jest and types

--- src/Slice.spec.ts
+++ src/Slice.spec.ts
@@ -1,57 +1,65 @@
-import { Slice } from './Slice';
+import { Slice } from './Slice'
 
 describe('Slice', () => {
-    it('should create a slice correctly', () => {
-        const slice = new Slice([1, 2, 3, 4, 5], 1, 3);
+  it('should create a slice correctly', () => {
+    const slice = new Slice([1, 2, 3, 4, 5], 1, 3)
 
-        expect(slice.length()).toBe(3);
-        expect(slice.at(0)).toBe(2);
-        expect(slice.at(1)).toBe(3);
-        expect(slice.at(2)).toBe(4);
-    });
+    expect(slice.length()).toBe(3)
+    expect(slice.at(0)).toBe(2)
+    expect(slice.at(1)).toBe(3)
+    expect(slice.at(2)).toBe(4)
+  })
 
-    it('should create a slice from an array correctly', () => {
-        const slice = Slice.from([1, 2, 3]);
+  it('should create a slice from an array correctly', () => {
+    const slice = Slice.from([1, 2, 3])
 
-        expect(slice.length()).toBe(3);
-        expect(slice.at(0)).toBe(1);
-        expect(slice.at(1)).toBe(2);
-        expect(slice.at(2)).toBe(3);
-    });
+    expect(slice.length()).toBe(3)
+    expect(slice.at(0)).toBe(1)
+    expect(slice.at(1)).toBe(2)
+    expect(slice.at(2)).toBe(3)
+  })
 
-    it('should iterate over slice', () => {
-        const slice = new Slice([1, 2, 3, 4, 5], 1, 3);
+  it('should iterate over slice', () => {
+    const slice = new Slice([1, 2, 3, 4, 5], 1, 3)
 
-        const result = [];
-        for (const item of slice) {
-            result.push(item);
-        }
+    const result = []
+    for (const item of slice) {
+      result.push(item)
+    }
 
-        expect(result).toEqual([2, 3, 4]);
-    });
+    expect(result).toEqual([2, 3, 4])
+  })
 
-    it('should map correctly', () => {
-        const slice = new Slice([1, 2, 3, 4, 5], 1, 3);
+  it('should map correctly', () => {
+    const slice = new Slice([1, 2, 3, 4, 5], 1, 3)
 
-        const mapped = slice.map((item) => item * 2);
+    const mapped = slice.map((item) => item * 2)
 
-        expect(mapped.at(0)).toBe(4);
-        expect(mapped.at(1)).toBe(6);
-        expect(mapped.at(2)).toBe(8);
-    });
+    expect(mapped.at(0)).toBe(4)
+    expect(mapped.at(1)).toBe(6)
+    expect(mapped.at(2)).toBe(8)
+  })
 
-    it('should be convertible to an array', () => {
-        const slice = new Slice([1, 2, 3, 4, 5], 1, 3);
+  it('should be convertible to an array', () => {
+    const slice = new Slice([1, 2, 3, 4, 5], 1, 3)
 
-        expect(Array.from(slice)).toEqual([2, 3, 4]);
-    });
+    expect(Array.from(slice)).toEqual([2, 3, 4])
+  })
 
-    it('should create a shallow copy', () => {
-        const slice1 = new Slice([1, 2, 3, 4, 5], 1, 3);
-        const slice2 = slice1.shallowCopy();
+  it('should create a shallow copy', () => {
+    const slice1 = new Slice([1, 2, 3, 4, 5], 1, 3)
+    const slice2 = slice1.shallowCopy()
 
-        expect(slice1.length()).toBe(slice2.length());
-        expect(slice1.at(0)).toBe(slice2.at(0));
-        expect(slice1).not.toBe(slice2);
-    });
-});
+    expect(slice1.length()).toBe(slice2.length())
+    expect(slice1.at(0)).toBe(slice2.at(0))
+    expect(slice1).not.toBe(slice2)
+  })
+
+  it('should reduce correctly', () => {
+    const slice1 = new Slice([1, 2, 3, 4, 5], 1, 3)
+
+    const sum = slice1.reduce((acc, cur) => acc + cur, 0)
+
+    expect(sum).toBe(9)
+  })
+})
--- src/Slice.ts
+++ src/Slice.ts
@@ -4,81 +4,96 @@
  * You can use shallowCopy() to create a copy of the slice.
  */
 
-export class Slice<T> implements Iterable<T> {
-    #buf: T[];
-    #start: number;
-    #end: number;
-    #length: number;
-
-    constructor(buf: T[], start: number, end?: number) {
-        this.#buf = buf;
-        this.#start = start;
-        this.#end = end ? end : buf.length;
-        this.#length = this.#end + 1 - this.#start;
-    }
-
-    *[Symbol.iterator]() {
-        for (let i = this.#start; i <= this.#end; i++) {
-            yield this.#buf[i];
-        }
-    }
-
-    static from<T>(array: ArrayLike<T>) {
-        return new Slice(array as T[], 0, array.length - 1);
-    }
-
-    length(): number {
-        return this.#length;
-    }
-
-    get = this.at.bind(this);
-    at(index: number): T {
-        if (index > this.#length - 1) {
-            throw new Error(`${index} is out of bounds`);
-        }
-
-        return this.#buf[this.#start + index];
-    }
-
-    last(): T {
-        return this.#buf[this.#end - 1];
-    }
-
-    first(): T {
-        return this.#buf[this.#start];
-    }
-
-    slice(start: number, end?: number): Slice<T> {
-        return new Slice(this.#buf, this.#start + start, end ? this.#start + end - 1 : this.#end);
-    }
-
-    map<U>(callback: (item: T, index?: number) => U): Slice<U> {
-        const buf: U[] = [];
-        for (let i = this.#start; i <= this.#end; i++) {
-            buf.push(callback(this.#buf[i], i));
-        }
-        return new Slice(buf, 0, buf.length);
-    }
-
-    /**
-     * Creates a shallow copy of the slice
-     */
-    shallowCopy(): Slice<T> {
-        const copy = [];
-        for (let i = this.#start; i <= this.#end; i++) {
-            copy.push(this.#buf[i]);
-        }
-
-        return new Slice(copy, 0, copy.length - 1);
-    }
-
-    join(separator?: string): string {
-        const arr: T[] = [];
-        for (let i = this.#start; i < this.#end; i++) {
-            arr.push(this.#buf[i]);
-        }
-        return arr.join(separator);
-    }
+function nonNullable<T> (arg: T): arg is NonNullable<T> {
+  return !!(arg ?? true)
 }
 
-export const sliceVec = <T>(array: T[], start: number, end: number): Slice<T> => new Slice(array, start, end);
+export class Slice<T> implements Iterable<T> {
+  #buf: T[]
+  #start: number
+  #end: number
+  #length: number
+  
+  constructor(buf: T[], start: number, end?: number) {
+    this.#buf = buf
+    this.#start = start
+    this.#end = end ? end : buf.length
+    this.#length = this.#end + 1 - this.#start
+  }
+
+  *[Symbol.iterator]() {
+    for (let i = this.#start; i <= this.#end; i++) {
+      yield this.#buf[i]
+    }
+  }
+
+  static from<T>(array: ArrayLike<T>, start?: number, end?: number) {
+    return new Slice(array as T[], start ?? 0, end ?? array.length - 1)
+  }
+
+  length(): number {
+    return this.#length
+  }
+
+  get = this.at.bind(this)
+  at(index: number): T {
+    if (index > this.#length - 1) {
+      throw new Error(`${index} is out of bounds`)
+    }
+
+    return this.#buf[this.#start + index]
+  }
+
+  last(): T {
+    return this.#buf[this.#end - 1]
+  }
+
+  first(): T {
+    return this.#buf[this.#start]
+  }
+
+  slice(start: number, end?: number): Slice<T> {
+    return new Slice(this.#buf, this.#start + start, end ? this.#start + end - 1 : this.#end)
+  }
+
+  map<U>(callback: (item: T, index?: number) => U): Slice<U> {
+    const buf: U[] = []
+    for (let i = this.#start; i <= this.#end; i++) {
+      buf.push(callback(this.#buf[i], i))
+    }
+    return new Slice(buf, 0, buf.length)
+    Array.prototype.reduce
+  }
+
+  reduce<U>(callback: (accumulator: U, currentValue: T, currentIndex: number, slice: Slice<T>) => U, initialValue: U): U {
+    let buf = initialValue
+        
+    for (let i = initialValue === undefined ? this.#start + 1 : this.#start; i <= this.#end; i++) {
+      buf = callback(buf, this.#buf[i], i, this)
+    }
+
+    return buf
+  }
+
+  /**
+     * Creates a shallow copy of the slice
+     */
+  shallowCopy(): Slice<T> {
+    const copy = []
+    for (let i = this.#start; i <= this.#end; i++) {
+      copy.push(this.#buf[i])
+    }
+
+    return new Slice(copy, 0, copy.length - 1)
+  }
+
+  join(separator?: string): string {
+    const arr: T[] = []
+    for (let i = this.#start; i < this.#end; i++) {
+      arr.push(this.#buf[i])
+    }
+    return arr.join(separator)
+  }
+}
+
+export const sliceVec = <T>(array: T[], start: number, end: number): Slice<T> => new Slice(array, start, end)
